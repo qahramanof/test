edfd4cd86bbba6711337d2a645de2a6faaf26f85
unknown <kananng@code.edu.az>
2023-04-17 14:35:16 +0400

    update

---
+++ sinifde task/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+
+
+int n = 0;
+int m = 10;
+int i = n;
+
+int sum = 0;
+while (i < m)
+
+{
+
+    if (i % 2 == 0)
+    {
+        sum += i;
+    }
+
+    i++;
+}
+Console.WriteLine(sum);
+
+
+
+
+
+//Console.WriteLine("sayi girin");
+//int say = int.Parse(Console.ReadLine());
+
+//int netice = say;
+//while (netice % 3 == 0)
+//{
+//    netice /= 3;
+//}
+//if (netice == 1)
+//{
+//    Console.WriteLine("quvvetidir");
+//}
+//else
+//{
+//    Console.WriteLine("quvvet deyil");
+//}
+
+
+
+
+
+
+
+
